bc5e9eb7e00703e9f7083bae1def1eedac2732a3
JanielleOliveira <janielleoliver.brito@gmail.com>
2024-11-11 21:51:07 -0300

    Adiciona configuração de CORS para permitir acesso de http://localhost:63342 em todos os endpoints

--- src/main/java/com/jane/crm/CrmApplication.java
+++ src/main/java/com/jane/crm/CrmApplication.java
@@ -2,12 +2,26 @@ package com.jane.crm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CrmApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrmApplication.class, args);
+	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**") // Permite CORS em todos os endpoints
+						.allowedOrigins("http://localhost:63342") // Define a origem permitida (front-end)
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS"); // Métodos permitidos
+			}
+		};
 	}
 
 }
